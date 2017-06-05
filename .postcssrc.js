2f8e930a48b04fddc35d932aa469510d55ff96cf
aoxiang78 <ao_xiang@live.com>
2017-06-05 10:22:30 +0800

    add postcss-salad

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,6 +3,21 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
+    "postcss-salad": {
+      "browsers": ["ie > 8", "last 2 versions"],
+      "features": {
+        "bem": {
+          "shortcuts": {
+            "component": "b",
+            "modifier": "m",
+            "descendent": "e"
+          },
+          "separators": {
+            "descendent": "__",
+            "modifier": "--"
+          }
+        }
+      }
+    }
   }
 }
